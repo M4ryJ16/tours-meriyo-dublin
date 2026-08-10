21034e0da951e0e22e658dfe733a92be73f30e18
David León <davidalfonsoleoncarmona@gmail.com>
2026-08-10 15:18:39 -0600

    Upload files

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  },
+  devToolbar: {
+      enabled: false
+  }
+});
